222f6c44a5fc5eed52b9691bc199836c90251cf8
dtvi2412 <dtvi2412@gmail.com>
2020-08-26 22:31:42 +0700

    Updates

---
+++ precache-manifest.03b706a0da3d669b80829c9fd74be1b0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e575a26e7895dd6a5e8a41a5f8725c47",
+    "url": "/React-in-github-demo/index.html"
+  },
+  {
+    "revision": "04dd5076faa730f1d8c6",
+    "url": "/React-in-github-demo/static/css/main.170193d7.chunk.css"
+  },
+  {
+    "revision": "680977abb7288c408a63",
+    "url": "/React-in-github-demo/static/js/2.4b91b5c6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-in-github-demo/static/js/2.4b91b5c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "04dd5076faa730f1d8c6",
+    "url": "/React-in-github-demo/static/js/main.67927759.chunk.js"
+  },
+  {
+    "revision": "d17e046549cea8f15b37",
+    "url": "/React-in-github-demo/static/js/runtime-main.d48c0aae.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.cfd7deab8f8660a995e678cf40ea9420.js"
+  "/React-in-github-demo/precache-manifest.03b706a0da3d669b80829c9fd74be1b0.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/React-in-github-demo/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
